ce9f884199c7f67c9fee599659255ae5d4635cb1
asceznyk <asceznyk@gmail.com>
2022-01-05 20:32:06 +0530

    ..

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 class FeatureLayerNorm(nn.Module):
     def __init__(self, n_feats):
         super(FeatureLayerNorm, self).__init__()
-        self.layer_norm = nn.LayerNorm(self.n_feats)
+        self.layer_norm = nn.LayerNorm(n_feats)
 
     def forward(self, x):
         x = x.transpose(2,3).contiguous() ## x.shape (batch, channel, time, feature)
