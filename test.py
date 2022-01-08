dacd9bb83fbe39e70f27c948f4ddce8b4db33af8
asceznyk <asceznyk@gmail.com>
2022-01-08 16:26:05 +0530

    ..

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ torch.manual_seed(7)
 
 x = torch.randn(8, 1, 128, 1024)
 cnn = nn.Conv2d(1, 32, 3, stride=1, padding=3//2)
-rescnn = ResCNN(32, 64, 3, 1, 0.5, 128)
+rescnn = ResCNN(32, 32, 3, 1, 0.5, 128)
 y = cnn(x)
 y = rescnn(y) 
 
