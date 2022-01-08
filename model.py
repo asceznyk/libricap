4aff508324dee45d15b722e83f4760b48fbc1cc4
asceznyk <asceznyk@gmail.com>
2022-01-08 19:56:11 +0530

    ..

--- model.py
+++ model.py
@@ -15,9 +15,9 @@ class FeatureLayerNorm(nn.Module):
         return x.transpose(2,3).contiguous() ## x.shape (batch, channel, feature, time)
 
 class ResCNN(nn.Module):
-    def __init__(self, in_c, out_c, kernel, stride, dropout, n_feats):
+    def __init__(self, inp_c, out_c, kernel, stride, dropout, n_feats):
         super(ResCNN, self).__init__()
-        self.cnn1 = nn.Conv2d(in_c, out_c, kernel, stride, padding=kernel//2)
+        self.cnn1 = nn.Conv2d(inp_c, out_c, kernel, stride, padding=kernel//2)
         self.cnn2 = nn.Conv2d(out_c, out_c, kernel, stride, padding=kernel//2)
         self.layer_norm1 = FeatureLayerNorm(n_feats)
         self.layer_norm2 = FeatureLayerNorm(n_feats)
@@ -32,8 +32,21 @@ class ResCNN(nn.Module):
         x = self.layer_norm2(x)
         x = self.dropout2(F.gelu(x))
         x = self.cnn2(x)
-        return x + res
+        return x + res ## x.shape (batch, channel, feature, time)
 
+class BiGRU(nn.Module):
+    def __init__(self, gru_dim, hidden_size, dropout, batch_first):
+        super(BiGRU, self).__init__()
+        self.bigru = nn.GRU(gru_dim, hidden_size, 
+                          num_layers=1, batch_first=batch_first, bidirectional=True)
+        self.layer_norm = nn.LayerNorm(gru_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = layer_norm(x)
+        x = F.gelu(x)
+        x, _ = self.bigru(x)
+        return self.dropout(x)
 
 
 
