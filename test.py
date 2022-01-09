13c29aa98514567b71afb2c78ada477afeaab72e
asceznyk <asceznyk@gmail.com>
2022-01-09 11:53:02 +0530

    ..

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from model import *
 torch.manual_seed(7)
 
 x = torch.randn(8, 1, 128, 1024)
-cnn = nn.Conv2d(1, 32, 3, stride=1, padding=3//2)
+cnn = nn.Conv2d(1, 32, 3, stride=2, padding=3//2)
 rescnn = ResCNN(32, 32, 3, 1, 0.5, 128)
 bigru = BiGRU(128, 256, 0.5, batch_first=True)
 
