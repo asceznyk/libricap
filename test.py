a6aac8797187fbd6a533f4efe3afeb450e170109
asceznyk <asceznyk@gmail.com>
2022-01-12 18:28:39 +0530

    ..

--- test.py
+++ test.py
@@ -1,23 +1,44 @@
+import argparse
+
 import numpy as np
 
 import torch
+import torch.optim as optim
+
 from torch.utils.data import Dataset, DataLoader
 
 import torchaudio
 
+from dataset import *
 from model import *
+from trainer import Trainer
 
-torch.manual_seed(7)
+def main(args):
+    torch.manual_seed(7)
 
-n_feats = 128
-gru_dim = 512
-x = torch.randn(8, 1, n_feats, 1000)
-speech_model = SpeechRecognizer(3, 5, 29, n_feats, gru_dim, 2)
-y = speech_model(x)
+    [specs, labels, ils, lls] = torch.load('/kaggle/input/speechsample/train_samp.pt') 
+    loader = DataLoader(TensorDataset(specs, labels, torch.Tensor(ils), torch.Tensor(lls)), 8)
 
-print(speech_model)
+    hparams = {
+        'n_res_layers': 3,
+        'n_gru_layers': 2,
+        'n_class': 29,
+        'n_feats': 128,
+        'gru_dim': 512 
+    }
 
-print(x, x.size())
-print(y, y.size())
+    model = SpeechRecognizer(**hparams)
 
+    trainer = Trainer(model, loader, None, args)
+    trainer.fit()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size')
+    parser.add_argument('--learning_rate', type=int, default=5e-4, help='learning rate')
+    parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train')
+    parser.add_argument('--ckpt_path', default='./state.best',  type=str, help='path of the model to be saved')
+       
+    options = parser.parse_args()
+    main(options)
 
