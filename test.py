c5de49288de311885af1a2b61fb0d4713fadc58e
asceznyk <asceznyk@gmail.com>
2022-01-16 13:19:08 +0530

    ..

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def main(args):
 
     hparams = {
         'n_res_layers': 3,
-        'n_gru_layers': 2,
+        'n_gru_layers': 5,
         'n_class': 29,
         'n_feats': 128,
         'gru_dim': 512 
