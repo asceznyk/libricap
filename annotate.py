530509d33f30fdd37f2be59cadc5263edb773fca
asceznyk <asceznyk@gmail.com>
2022-02-04 17:42:44 +0530

    ..

--- annotate.py
+++ annotate.py
@@ -16,16 +16,11 @@ class SpeechRecognizer:
         self.model = torch.jit.load(model_file)
         self.model.eval().to('cpu')
 
-        self.hidden = (torch.zeros(1,1,1024), torch.zeros(1,1,1024))
-        self.featurizer = get_featurizer(8000) 
-
     def annotate(self, wave_file):
         with torch.no_grad():
             waveform, _  = torchaudio.load(wave_file)
-            mel_spec = valid_audio_transforms(waveform).unsqueeze(0) ##(1,1,feature,time)
-            outputs, (ht, ct) = self.model(mel_spec, self.hidden)
-            outputs = torch.nn.functional.softmax(outputs, dim=2) 
-            text, _ = greedy_decoder(outputs)
+            outputs, _ = self.model(valid_audio_transforms(waveform).unsqueeze(0)) 
+            text, _ = greedy_decoder(torch.nn.functional.softmax(outputs, dim=2))
 
             print(" ".join(text.split()))
 
